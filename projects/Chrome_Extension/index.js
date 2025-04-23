37cdc65cdddb0f693ee4acb3d67e5dc8b08004af
josemariasava-opit <josemaria.s@students.opit.com>
2025-04-23 16:35:45 +0200

    final version of chrome extension

--- projects/Chrome_Extension/index.js
+++ projects/Chrome_Extension/index.js
@@ -2,7 +2,47 @@ let myLeads = []
 const inputEl = document.getElementById("input-el") // const : cannot be reassigned
 const inputBtn = document.getElementById("input-btn") // grap button element from html
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function () {
+    // Grab the url of the current tab -- chrome API
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a href="${leads[i]}" target="_blank">
+                    ${leads[i]}
+                </a>
+            </li>`;
+    }
+    ulEl.innerHTML = listItems
+}
+
+// Delete button 
+deleteBtn.addEventListener("dblclick", function () {
+    // Clear the localstorage   
+    localStorage.clear()
+    // Clear the array of leads
+    myLeads = []
+    render(myLeads)
+})
 
 
 inputBtn.addEventListener("click", function () {
@@ -10,19 +50,8 @@ inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-
-    renderLeads()
+    render(myLeads)
 })
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a href="${myLeads[i]}" target="_blank">
-                    ${myLeads[i]}
-                </a>
-            </li>`;
-    }
-    ulEl.innerHTML = listItems
-}
+
+
